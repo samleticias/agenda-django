b8193219f0110765d0b32477b85eaa483155427a
samleticias <sammyaleticia09@gmail.com>
2025-06-24 01:40:29 -0300

    cria o model Category e faz ligação com Contact

--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,22 @@ from django.utils import timezone
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -16,3 +28,13 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
